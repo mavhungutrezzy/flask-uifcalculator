9a38e203fd455dca3194cbce7010418b1f0d88c5
mavhungutrezzy <adivhahomavhungu@outlook.com>
2025-11-28 15:54:57 +0200

    Add robots.txt from static folder

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app, send_from_directory
 
 home_bp = Blueprint("home", __name__)
 
@@ -6,3 +6,9 @@ home_bp = Blueprint("home", __name__)
 @home_bp.route("/")
 def index():
     return render_template("home.html")
+
+
+@home_bp.route("/robots.txt")
+def robots_txt():
+    """Serve the robots.txt file from the static directory."""
+    return send_from_directory(current_app.static_folder, "robots.txt", mimetype="text/plain")
